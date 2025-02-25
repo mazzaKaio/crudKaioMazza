12a451d6674713c5d44f46e6ef8c17d59e9ffc3d
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-25 20:58:03 -0300

    Create kaioMazza_db.sql

---
+++ kaioMazza_db.sql
@@ -0,0 +1,10 @@
+create database kaioMazza_db
+
+create table usuario(
+	idusuario int not null auto_increment,
+    nome text,
+    telefone text,
+    usuario text,
+    senha text,
+    primary key (idusuario)    
+);
