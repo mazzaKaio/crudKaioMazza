b052a5d7346bfeb28bea61623c7cc473f504f3af
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-27 20:42:15 -0300

    100%

--- crud.py
+++ crud.py
@@ -29,10 +29,10 @@ def read_users():
 
     return result
 
-def update_user(userid, nome, telefone, email, usuario, senha):
+def update_user(nome, telefone, email, usuario, senha, userid):
     conn = get_connection()
     cursor = conn.cursor()
-    query = "UPDATE usuario SET nome=%s, telefone=%s, email=%s, usuario'=%s, senha=%s WHERE userid = %s"
+    query = "UPDATE usuario SET nome = %s, telefone = %s, email = %s, usuario = %s, senha = %s WHERE userid = %s"
     cursor.execute(query, (nome, telefone, email, usuario, senha, userid))
     conn.commit()
     cursor.close()
